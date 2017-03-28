d069c2826a33c38f609ab25e1983f6de39cb1e8f
William Zhang <will800228@gmail.com>
2017-03-28 13:48:18 -0400

    use array instead of buffer to check

--- src/Router.java
+++ src/Router.java
@@ -177,7 +177,7 @@ public class Router {
         while (true) {
             byte[] data = this.receivePacket();
             ByteBuffer byteBuffer = ByteBuffer.wrap(data);
-            if (byteBuffer.getInt(8) == 0) {
+            if (data[8] == 0) {
                 // receive hello
                 for (int i = 0; i < circuitDbs[id - 1].nLinks; i++) {
                     PacketHello packetHello = new PacketHello();
