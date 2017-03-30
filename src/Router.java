0a0f7e560be3ec6bff94fbc9cc8e295dbc34998a
William Zhang <will800228@gmail.com>
2017-03-30 18:32:47 -0400

    add comments

--- src/Router.java
+++ src/Router.java
@@ -12,7 +12,7 @@ public class Router {
 
     private static final int INF = 65535;
     private static final int NUMBER_OF_ROUTERS = 5;
-    private static final int DELAY = 10000; // program automatic termination time
+    private static final int DELAY = 30000; // program automatic termination time
     private int id; // id of this router
     private DatagramSocket nseSocket;
     private InetAddress nseHost;
@@ -22,7 +22,16 @@ public class Router {
     private Map<Integer, List<Integer>> duplicateTracker = new HashMap<>();
     private PrintWriter logWriter;
 
-    public void printTopologyDatabase() {
+    /**
+     * release resources
+     */
+    private void close() {
+        if (logWriter != null) {
+            logWriter.close();
+        }
+    }
+
+    private void printTopologyDatabase() {
         logWriter.println("# Topology Database");
         for (int i = 0; i < NUMBER_OF_ROUTERS; i++) {
             CircuitDb circuitDb = circuitDbs[i];
@@ -32,7 +41,7 @@ public class Router {
                 logWriter.printf("R%d -> R%d link %d cost %d\n", id, i + 1, linkCost.link, linkCost.cost);
             }
         }
-        dijkstra();
+        dijkstra(); // only compute shortest path if topology database has changed
     }
 
     private void dijkstra() {
@@ -53,21 +62,22 @@ public class Router {
                 int link = lc.link;
                 if (linkToRouterMap.containsKey(link)) {
                     int j = linkToRouterMap.get(link);
+                    // connect nodes
                     adjacencyGraph.get(i).add(new int[]{j, lc.cost});
                     adjacencyGraph.get(j).add(new int[]{i, lc.cost});
                     if (i == id - 1) {
-                        newRib[j] = lc.cost;
+                        newRib[j] = lc.cost; // adjacent to the source
                     } else if (j == id - 1) {
-                        newRib[i] = lc.cost;
+                        newRib[i] = lc.cost; // adjacent to the source
                     }
                 } else {
                     linkToRouterMap.put(link, i);
                 }
             }
         }
-
-        DefaultMutableTreeNode pathTreeRoot = new DefaultMutableTreeNode(id - 1);
-        DefaultMutableTreeNode[] nodes = new DefaultMutableTreeNode[NUMBER_OF_ROUTERS];
+        // build path tree
+        DefaultMutableTreeNode pathTreeRoot = new DefaultMutableTreeNode(id - 1); // need to write my own tree class when have time
+        DefaultMutableTreeNode[] nodes = new DefaultMutableTreeNode[NUMBER_OF_ROUTERS]; // keep track of all tree nodes
         Set<DefaultMutableTreeNode> p = new LinkedHashSet<>();
         for (int i = 0; i < NUMBER_OF_ROUTERS; i++) {
             if (i == id - 1) {
@@ -79,12 +89,12 @@ public class Router {
             }
         }
         while (!p.isEmpty()) {
-            // find w not in N' such that D(w) is a minimum
+            // find w not in p such that D(w) is a minimum
             p.stream().sorted((a, b) -> newRib[(Integer) a.getUserObject()] - newRib[(Integer) b.getUserObject()])
                     .limit(1)
                     .forEach(w -> {
                         p.remove(w);
-                        // update D(v) for all v adjacent to w and not in N' :
+                        // update D(v) for all v adjacent to w and not in p :
                         int routerId = (int) w.getUserObject();
                         for (int[] neighbor : adjacencyGraph.get(routerId)) {
                             int neighborId = neighbor[0];
@@ -102,9 +112,9 @@ public class Router {
         for (int i = 0; i < NUMBER_OF_ROUTERS; i++) {
             logWriter.printf("R%d -> R%d -> ", id, i + 1);
             if (i + 1 == id) {
-                logWriter.println("local, 0");
+                logWriter.println("local, 0"); // self
             } else if (newRib[i] == INF){
-                logWriter.println("INF, INF");
+                logWriter.println("INF, INF"); // unconnected router
             } else {
                 logWriter.printf("R%d, %d\n", (int)(nodes[i].getUserObjectPath()[1]) + 1, newRib[i]);
             }
@@ -113,7 +123,7 @@ public class Router {
     }
 
     public Router(int id, InetAddress nseHost, int nsePort, int routerPort) throws IOException {
-        this.logWriter = new PrintWriter(new FileOutputStream("router" + id + ".log"), true);
+        this.logWriter = new PrintWriter(new FileOutputStream("router" + id + ".log"), true); // automatically flushed
         this.nseSocket = new DatagramSocket(routerPort);
         this.id = id;
         this.nseHost = nseHost;
@@ -133,7 +143,7 @@ public class Router {
         byte[] data = packetInit.toBytes();
         DatagramPacket datagramPacket = new DatagramPacket(data, data.length, nseHost, nsePort);
         nseSocket.send(datagramPacket);
-        logWriter.println("R" + id + " sends an INIT: routerId " + id);
+        logWriter.println("R" + id + " sends an INIT: router_id " + id);
     }
 
     private byte[] receivePacket() throws IOException {
@@ -163,7 +173,7 @@ public class Router {
             byte[] data = packetHello.toBytes();
             DatagramPacket datagramPacket = new DatagramPacket(data, data.length, nseHost, nsePort);
             nseSocket.send(datagramPacket);
-            logWriter.println("R" + id + " sends an Hello: routerId " + id + " linkId " + linkCost.link);
+            logWriter.println("R" + id + " sends a Hello: router_id " + id + " link_id " + linkCost.link);
         }
     }
 
@@ -175,11 +185,11 @@ public class Router {
                 System.exit(0);
             }
         }, DELAY);
-        // receive new LSPDU
+        // receive new packet after receiving circuit database and sending hello
         while (true) {
             byte[] data = this.receivePacket();
             ByteBuffer byteBuffer = ByteBuffer.wrap(data);
-            boolean isHello = byteBuffer.getInt(8) == 0;
+            boolean isHello = byteBuffer.getInt(8) == 0; // hello has 8 bytes, LS PDU has 20 bytes
             byteBuffer.position(0);
             if (isHello) {
                 // receive hello
@@ -196,7 +206,7 @@ public class Router {
                         logWriter.printf("R%d sends an LS PDU: sender %d, router_id %d, link_id %d, cost %d, via %d\n",
                                 id, id, routerId, linkCost.link, linkCost.cost, packetHello.link);
                     }
-                    routerId++;
+                    routerId ++;
                 }
             } else {
                 // received LSPDU
@@ -252,8 +262,8 @@ public class Router {
             iae.printStackTrace();
             System.exit(1);
         } finally {
-            if (router != null && router.logWriter != null) {
-                router.logWriter.close();
+            if (router != null) {
+                router.close();
             }
         }
     }
